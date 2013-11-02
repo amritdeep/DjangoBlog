8ec20acf511257ef2c1c0b46b4803bc4a088c0c1
Amrit Deep Dhungana <amritdeepdhungana@Amrits-MacBook-Pro.local>
2013-11-02 22:35:01 +0545

    add static css and images to project alone with template

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -69,6 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    ('assets', "/Users/amritdeepdhungana/venvpython/DjangoBlog/static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
