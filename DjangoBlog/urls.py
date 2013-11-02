dc9938623891a172da2109654a153262d50e591d
Amrit Deep Dhungana <amritdeepdhungana@Amrits-MacBook-Pro.local>
2013-11-02 20:23:09 +0545

    add template file in templates folder for viewing article

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^articles/', include('article.urls')),
     # Examples:
     # url(r'^$', 'DjangoBlog.views.home', name='home'),
     # url(r'^DjangoBlog/', include('DjangoBlog.foo.urls')),
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
