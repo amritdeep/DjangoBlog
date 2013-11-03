32d890ddfb1fd8d1a056ddc0e74d09f60aaec25f
Amrit Deep Dhungana <amritdeepdhungana@Amrits-MacBook-Pro.local>
2013-11-04 00:50:08 +0545

    add login and logout functionalites or features

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -15,4 +15,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/login/$', 'DjangoBlog.views.login'),
+    url(r'^accounts/auth/$', 'DjangoBlog.views.auth_view'),
+    url(r'^accounts/logout/$', 'DjangoBlog.views.logout'),
+    url(r'^accounts/loggedin$', 'DjangoBlog.views.loggedin'),
+    url(r'^accounts/invalid/$', 'DjangoBlog.views.invalid_login'),
+
 )
